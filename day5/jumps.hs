3a31273d4230a2d34894497e93f6cd6ea23adf97
Lennart Weckeck <lennart.weckeck@mailbox.org>
2017-12-06 22:27:24 +0100

    Added solutions for day 5

---
+++ day5/jumps.hs
@@ -0,0 +1,19 @@
+import Data.Vector (Vector)
+import qualified Data.Vector as V
+import Data.Maybe (isJust)
+
+outOfBounds :: Int -> (Int, Int) -> Bool
+outOfBounds x (lower, upper) = x < lower || x > upper
+
+advance :: Monad m => (Int, Vector Int) -> Int -> m (Int, Vector Int)
+advance (pos, jumplist) offset = return $ (pos + offset, jumplist V.// [(pos, offset + 1)])
+
+jump :: (Int, Vector Int) -> Maybe (Int, Vector Int)
+jump state@(pos, jumplist) = (jumplist V.!? pos) >>=
+  (\offset -> return (pos + offset, jumplist V.// [(pos, offset + 1)]))
+
+jumpsUntilOutOfBounds :: [Int] -> Int
+jumpsUntilOutOfBounds jumplist = length $ init $ takeWhile isJust $ iterate (>>= jump) $ pure (0, V.fromList jumplist)
+
+main :: IO ()
+main = interact $ (++ "\n") . show . jumpsUntilOutOfBounds . (map read) . lines
