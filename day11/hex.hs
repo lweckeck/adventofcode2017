19dc0ffb45d0740aa16323a1909808b6def8fd36
Lennart Weckeck <lennart.weckeck@mailbox.org>
2017-12-13 21:30:04 +0100

    Added solutions for day 11

---
+++ day11/hex.hs
@@ -0,0 +1,19 @@
+import qualified Data.List.Split as S
+import Data.Char (isSpace)
+
+hexMove :: (Int, Int, Int) -> String -> (Int, Int, Int)
+hexMove (x,y,z) "n"  = (x,   y+1, z-1)
+hexMove (x,y,z) "ne" = (x+1, y,   z-1)
+hexMove (x,y,z) "se" = (x+1, y-1, z)
+hexMove (x,y,z) "s"  = (x,   y-1, z+1)
+hexMove (x,y,z) "sw" = (x-1, y,   z+1)
+hexMove (x,y,z) "nw" = (x-1, y+1, z)
+
+hexDist :: (Int, Int, Int) -> Int
+hexDist (x,y,z) = (abs x + abs y + abs z) `div` 2
+
+parseMoves :: String -> (Int, Int, Int)
+parseMoves = foldl hexMove (0,0,0) . (S.splitOn ",")
+
+main :: IO ()
+main = interact $ (++"\n") . show . hexDist . parseMoves . (filter (not . isSpace))
